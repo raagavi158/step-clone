15ea8a969b652a7d094bb0ce570f97b9a9c797e3
Raagsvi <raagavi158@gmail.com>
2020-06-19 22:50:04 +0400

    Take an array of 4 strings as inout from the use and display each in a list using post and get requests

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -25,26 +25,41 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-    private List<String> quotes;
-    @Override
+    private List<String> quotes = new ArrayList<>();
+    /*@Override
     public void init() {
-        quotes = new ArrayList<>();
+        quotes = 
         quotes.add("2014 Forest Hill Drive - J.Cole");
         quotes.add("ASTROWORLD - Travis Scott");
         quotes.add("Blonde - Frank Ocean");
         quotes.add("Currents - Tame Impala");
-    }
+    }*/
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String json = convertToJsonUsingGson();
         response.setContentType("application/json;");
         response.getWriter().println(json);
     }
+     @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // If the user sends another POST request after the game is over, then start a new game.
+
+        // Get the input from the form.
+        String text = request.getParameter("player-choice");
+        String[] words = text.split("\\s*,\\s*");
+        for(int i = 0; i < 4; i++) {
+            quotes.add(words[i]);
+        }
+    // Redirect back to the HTML page.
+    response.sendRedirect("/music.html");
+    }
 
     private String convertToJsonUsingGson() {
         String json = "[";
         json += "\"" + quotes.get(0) +"\", \"" + quotes.get(1) + "\", \"" + quotes.get(2) + "\", \"" + quotes.get(3) +"\"";
         json += "]";
+        System.out.println(json);
         return json;
+        
     }
 }

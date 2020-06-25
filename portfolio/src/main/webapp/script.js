34daf15b855b124d2525dd7a0f213c940bf18aac
Raagsvi <raagavi158@gmail.com>
2020-06-25 15:29:21 +0400

    Made changes according to comments left on PR

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -18,17 +18,46 @@
 function genAlbums() {
     var past = new Set();
     
-    const albumNames = ['2014 Forest Hill Drive - J.Cole', 'ASTROWORLD - Travis Scott', 'Blonde - Frank Ocean', 'Currents - Tame Impala', 'Dont Forget About Me - Dominic Fike', 'GINGER - BROCKHAMPTON', 'Lyrics To Go Vol.1 - Kota the Friend', 
-    'Mind the Moon - Milky Chance', 'Oasis - Bad Bunny', 'Section.80 - Kendrick Lamar', 'The Divine Feminine - Mac Miller', 'The Click - AJR', 'YHLQMDLG - Bad Bunny', 'Ego Death - The Internet', 'The Getaway - Red Hot Chilli Peppers', 'Ved-Ritviz'];
-    const albumArt = ['2014FHD.png', 'ASTROWORLD.png', 'Blonde.png', 'Currents.png', 'DFAM.png', 'GINGER.png', 'LTG1.png', 'MTM.png', 'Oasis.png', 'Section80.png', 'TDF.png', 'TheClick.png', 'YHLQMDLG.png', 'ED.png', 'TG.png', 'Ved.png']
+    const albumNames = ['2014 Forest Hill Drive - J.Cole', 
+                        'ASTROWORLD - Travis Scott', 
+                        'Blonde - Frank Ocean', 
+                        'Currents - Tame Impala', 
+                        'Dont Forget About Me - Dominic Fike', 
+                        'GINGER - BROCKHAMPTON', 
+                        'Lyrics To Go Vol.1 - Kota the Friend', 
+                        'Mind the Moon - Milky Chance', 
+                        'Oasis - Bad Bunny', 
+                        'Section.80 - Kendrick Lamar', 
+                        'The Divine Feminine - Mac Miller', 
+                        'The Click - AJR', 'YHLQMDLG - Bad Bunny', 
+                        'Ego Death - The Internet', 
+                        'The Getaway - Red Hot Chilli Peppers', 
+                        'Ved-Ritviz'];
+    const albumArt = ['2014FHD.png', 
+                    'ASTROWORLD.png', 
+                    'Blonde.png', 
+                    'Currents.png', 
+                    'DFAM.png', 
+                    'GINGER.png', 
+                    'LTG1.png', 
+                    'MTM.png', 
+                    'Oasis.png', 
+                    'Section80.png', 
+                    'TDF.png', 
+                    'TheClick.png', 
+                    'YHLQMDLG.png', 
+                    'ED.png', 
+                    'TG.png', 
+                    'Ved.png']
     var ul = document.getElementById("dynamic-list");
     ul.innerHTML = "";
-    for(var i = 1; i < 5; i++) {
+    var numContainers = 5;
+    for (var i = 1; i < numContainers; i++) {
         var imageIndex; 
         do {
             imageIndex = Math.floor(Math.random() * albumArt.length);
         }
-        while(past.has(imageIndex));
+        while (past.has(imageIndex));
         past.add(imageIndex);
         const imgUrl = '/images/' + albumArt[imageIndex];
         const imgElement = document.createElement('img');
